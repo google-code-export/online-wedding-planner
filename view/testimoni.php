4e34d3fe69f7e056c688e7b23fab267b22caad6e
nez817 <nez817@8b6feb66-0111-bda6-36b2-7681229f614b>
2012-11-25 15:07:08 +0000

    pindahan home.php, pilihgedung.php, dan testimoni.php dari folder root

---
+++ view/testimoni.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Online Wedding Planner</title>
+    <!-- Bootstrap -->
+    <link href="../lib/css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  
+  <body>
+	<center>
+	<body body bgcolor="f8f5fa">
+	<div style="margin: 10px 10px 0px 10px;">
+	<div id="header"> <img src="../lib/img/owp_header.png">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="../lib/js/bootstrap.js"></script>
+	</div>
+	</center>
+	
+	<div style="margin: 15px 70px 5px 65px;">
+    
+		<div class="container-fluid">
+		<div class="row-fluid">
+		<div class="span4">
+		<ul class="nav nav-pills nav-stacked">
+			<li> <a href="home.php">Home</a> </li>
+			<li class="active"><a href="testimoni.php">Testimoni</a></li>
+			<li><a href="#">Promo</a></li>
+			<li><a href="aturjadwal.php">Jadwal Pernikahan</a></li>
+		
+			<form class="navbar-search pull-left">
+			<input type="text" class="search-query" placeholder="Search">
+			</form> </ul>
+			
+			<a href="pilihgedung.php" class="thumbnail">
+			<img src="../lib/img/rencana.png" alt="">
+			</a>
+		
+		</div>
+		
+		<div class="span8">
+		<!--tempat testimoni-->
+			<center><img src="../lib/img/testimoni_header.png"> </center><br>
+			<form class="form-horizontal">
+			Your Name	: <input type="text">
+			Your Email	: <input type="text"> </form>
+			<center><textarea rows="6" class="span12" placeholder="Type testimony...">
+			</textarea> </center>
+			<button type="submit" class="btn pull-right">Submit</button> <br>
+			<div class="pagination pagination-right">
+			<ul>
+			<li><a href="#">Prev</a></li>
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">Next</a></li>
+			</ul> </div>
+			
+		<ul class="media-list">
+		<body body bgcolor="#CCFF33">
+		<li class="media">
+		<a class="pull-left" href="#">
+		<img class="media-object" src="http://placehold.it/64x64">
+		</a>
+		<div class="media-body">
+		<h4 class="media-heading">Media heading</h4>
+		tes tes tes
+		</div></li> </ul> </body>
+		
+		</div>
+		</div>
+		</div>
