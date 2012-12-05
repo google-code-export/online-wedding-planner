feaccc8604eb5c009b8f2466cb5009e5fd58d36a
nez817 <nez817@8b6feb66-0111-bda6-36b2-7681229f614b>
2012-12-05 02:08:24 +0000

    Issue 3: Testimoni | nambah php di tampilan testimoni.php

--- view/testimoni.php
+++ view/testimoni.php
@@ -11,7 +11,7 @@
 	<body body bgcolor="f8f5fa">
 	<div style="margin: 10px 10px 0px 10px;">
 	<div id="header"> <img src="../lib/img/owp_header.png">
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <!--<script src="http://code.jquery.com/jquery-latest.js"></script>-->
     <script src="../lib/js/bootstrap.js"></script>
 	</div>
 	</center>
@@ -31,45 +31,42 @@
 			<input type="text" class="search-query" placeholder="Search">
 			</form> </ul>
 			
-			<!--form class="form-horizontal">
-			<center><img src="../lib/img/login.png"></center> <br>
-  			<div class="control-group">
-    		<!label class="control-label" for="inputEmail" class="pull-left">Email</label>
-    		<div class="controls">
-      		<input type="text" id="inputEmail" placeholder="Email">
-    		</div>
-  			</div>
-  			<div class="control-group">
-    		<label class="control-label" for="inputPassword">Password</label>
-    		<div class="controls">
-      		<input type="password" id="inputPassword" placeholder="Password">
-    		</div>
-  			</div>
-  			<div class="control-group">
-    		<div class="controls">
-      		<label class="checkbox">
-        <input type="checkbox"> Remember me
-      </label>
-      <button type="submit" class="btn">Sign in</button>
-    </div>
-  </div>
-</form-->
 			
 			<a href="pilihgedung.php" class="thumbnail">
 			<img src="../lib/img/rencana.png" alt="">
 			</a>
 		
 		</div>
-		
+<?php
+//include_once('themes/head.php');	
+//include_once('../testimoniController.php');
+	
+	 
+?>
+<?php
+	if(isset($error))
+	{
+		echo '<div class="alert alert-error" id="alert">Salah ! '.$error.' 
+			<button type="button" class="close" data-dismiss="alert" id="close">×</button></div>';
+	}
+	if(isset($success))
+	{
+		echo '<div class="alert alert-success">'.$success.'</div>';
+	}
+	 
+		else
+		{
+	?>		
 		<div class="span8">
 		<!--tempat testimoni-->
 			<center><img src="../lib/img/testimoni_header.png"> </center><br>
-			<form class="form-horizontal">
-			Your Name	: <input type="text">
-			Your Email	: <input type="text"> </form>
-			<center><textarea rows="6" class="span12" placeholder="Type testimony...">
+			<form class="form-horizontal"method="post" action="#">
+			Your Name	: <input type="text" name="nama" id="inputNama" placeholder="Nama">
+			Your Email	: <input type="email" name="email" id="inputEmail" placeholder="Email"> </form>
+			<center><textarea rows="6" class="span12" input type="text" name="isi_testi" id="inputIsi" placeholder="Isi testimoni...">
 			</textarea> </center>
-			<button type="submit" class="btn pull-right">Submit</button> <br>
+			<button type="submit" name="simpan_testimoni" class="btn pull-right" id="daftar_btn">Submit</button>
+			<button type="batal" name="batal" class="btn pull-right" id="batal">Batal</button>			<br>
 			<div class="pagination pagination-right">
 			<ul>
 			<li><a href="#">Prev</a></li>
@@ -78,12 +75,24 @@
 			<li><a href="#">3</a></li>
 			<li><a href="#">Next</a></li>
 			</ul> </div>
-			
+
+		<!--daftar testimoni-->	
+		<div id="testimoniList">
 		<table class="table table-striped">
+
 		<thead>
 		</thead>
+	<?php	
+	if($data!=0 )
+	{
+	foreach($data as $rows)
+	{
+	
+	?> 
   <tbody>
       <tr>
+	  	
+
 		<td><ul class="media-list">
 		<body body bgcolor="#CCFF33">
 		<li class="media">
@@ -91,11 +100,23 @@
 		<img class="media-object" src="http://placehold.it/64x64">
 		</a>
 		<div class="media-body">
-		<h4 class="media-heading">Media heading</h4>
-		tes tes tes
+		<h4 class="media-heading"><?php echo $rows['nama']; ?></h4>
+		<?php echo $rows['isi_testi']; ?>
 		</td></div></li> </ul> </body>
 	  </tr>
-	  <tr>
+	<?php
+		}
+	}
+	else
+	{
+	?>
+		<tr>
+			<td colspan="2">Testimoni Tidak Ada</td>
+		</tr>
+	<?php
+	}
+	?>
+	  <!--<tr>
 		<td><ul class="media-list">
 		<body body bgcolor="#CCFF33">
 		<li class="media">
@@ -118,15 +139,19 @@
 		<h4 class="media-heading">Media heading</h4>
 		tes tes tes
 		</div></li> </ul> </td> </body>
-	  </tr>
+	  </tr>-->
   </tbody>
 		 </body> </table>
+		 </div>
+
+<script type="text/javascript">
+	var url;
+	{
+		$('#testimoniList').show();
+		 	 		return false;
+	}
+</script>
 		
-		
-		
-		</div>
-		</div>
-		</div>
 		
 		<center><img src="../lib/img/adver.png"> <br> <br> </div></center>
 		<div style="margin: 15px 70px 5px 65px;">
@@ -165,7 +190,7 @@
 			</div> </ul>
 		</div>
 		
-		</div>
+		</div></div></div>
 		
 		<div style="margin: 10px 10px 0px 10px;">
 	<div id="footer"> <img src="../lib/img/footer.png">
@@ -175,3 +200,7 @@
   <li><a href="#">Peta Kantor</a></li>
 </ul>
 <img src="../lib/img/footer.png">
+
+<?php
+	}
+?>
