8921a1aac086c23614e86c73c9db4062aae86781
endhitaayu@gmail.com <endhitaayu@gmail.com@8b6feb66-0111-bda6-36b2-7681229f614b>
2012-12-09 04:45:30 +0000

    Menambah pilihetnik.php

---
+++ view/pilihetnik.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Online Wedding Planner</title>
+    <!-- Bootstrap -->
+    <link href="../lib/css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  
+  <body>
+	<center>
+	<body body bgcolor="f8f5fa">
+	<div style="margin: 10px 10px 0px 10px;">
+	<div id="header"> <img src="../lib/img/owp_header.png">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="../lib/js/bootstrap.js"></script>
+	</div>
+	</center>
+	
+	<div style="margin: 15px 70px 5px 65px;">
+    
+		<div class="container-fluid">
+		<div class="row-fluid">
+		<div class="span4">
+		<ul class="nav nav-pills nav-stacked">
+			<li> <a href="home.php">Home</a> </li>
+			<li><a href="testimoni.php">Testimoni</a></li>
+			<li><a href="#">Promo</a></li>
+			<li><a href="aturjadwal.php">Jadwal Pernikahan</a></li>
+		
+			<form class="navbar-search pull-left">
+			<input type="text" class="search-query" placeholder="Search">
+			</form> </ul>
+			
+			<a href="pilihgedung.php" class="thumbnail">
+			<img src="../lib/img/rencana.png" alt="">
+			</a>
+		
+		</div>
+		
+		<div class="span8">
+		
+		<div style="margin: 15px 0px 5px 70px;">
+		<ul class="thumbnails">
+		<li class="span2">
+		<a href="pilihgedung.php" class="thumbnail">
+		<img src="../lib/img/ic1.png" alt="">
+		  <h6><center>Pilih Gedung</center></h6>
+		</a>
+		</li>
+		
+		<li class="span2">
+		<a href="pilihetnik.php" class="thumbnail disabled">
+		<img src="../lib/img/ic2.png" alt="">
+		  <h6><center>Pilih Etnik</center></h6>
+		</a>
+		</li>
+		
+		<li class="span2">
+		<a href="aturpaket.php" class="thumbnail">
+		<img src="../lib/img/ic3.png" alt="">
+		  <h6><center>Atur Paket</center></h6>
+		</a>
+		</li>
+		
+		<li class="span2">
+		<a href="aturjadwal.php" class="thumbnail">
+		<img src="../lib/img/ic4.png" alt="">
+		  <h6><center>Atur Jadwal</center></h6>
+		</a>
+		</li>
+		
+		<li class="span2">
+		<a href="pilihetnik.php" class="thumbnail">
+		<img src="../lib/img/ic5.png" alt="">
+		  <h6><center>Lewati Langkah Ini</center></h6>
+		</a>
+		</li>
+</ul> </div>
+
+		<table border="0" width="100%" cellpadding="0" cellspacing = "0">
+		<!--thumbnail pilih gedung-->
+			<td width="50%" valign="top">
+			<div class="row-fluid">
+			<ul class="thumbnails">
+			<li class="span11">
+			<div class="thumbnail">
+			<img src="http://placehold.it/360x270" alt="">
+			<h3 align="center">Pelaminan Adat Minang</h3>
+			<p align="center">Pelaminan ini ... Harga Rp. 10 juta</p>
+			<p> <a class="btn btn-block" href="#">Details</a> <a class="btn btn-block btn-primary" href="./view/hero.php">Pilih</a></p>
+			</div>
+			</li>
+			</ul>
+			</div>
+			</td>
+			
+			<td width="50%" valign="top">
+			<div class="row-fluid">
+			<ul class="thumbnails">
+			<li class="span11">
+			<div class="thumbnail">
+			<img src="http://placehold.it/360x270" alt="">
+			<h3 align="center">Pelaminan Adat Minang</h3>
+			<p align="center">Pelaminan ini ... Harga Rp. 10 juta</p>
+			<p> <a class="btn btn-block" href="#">Details</a> <a class="btn btn-block btn-primary" href="./view/hero.php">Pilih</a></p>
+			</div>
+			</li>
+			</ul>
+			</div>
+			</td>
+			
+			<table border="0" width="100%" cellpadding="0" cellspacing = "0">
+			<td width="50%" valign="top">
+			<div class="row-fluid">
+			<ul class="thumbnails">
+			<li class="span11">
+			<div class="thumbnail">
+			<img src="http://placehold.it/360x270" alt="">
+			<h3 align="center">Pelaminan Adat Minang</h3>
+			<p align="center">Pelaminan ini ... Harga Rp. 10 juta</p>
+			<p> <a class="btn btn-block" href="#">Details</a>  <a class="btn btn-block btn-primary" href="./view/hero.php">Pilih</a></p>
+			</div>
+			</li>
+			</ul>
+			</div>
+			</td>
+			
+			<td width="50%" valign="top">
+			<div class="row-fluid">
+			<ul class="thumbnails">
+			<li class="span11">
+			<div class="thumbnail">
+			<img src="http://placehold.it/360x270" alt="">
+			<h3 align="center">Pelaminan Adat Minang</h3>
+			<p align="center">Pelaminan ini ... Harga Rp. 10 juta</p>
+			<p> <a class="btn btn-block" href="#">Details</a> <a class="btn btn-block btn-primary" href="./view/hero.php">Pilih</a></p>
+			</div>
+			</li>
+			</ul>
+			</div>
+			</td>
+			
+		</table>
+		</table>
+	</div>
+	
+	<div style="margin: 10px 10px 0px 10px;">
+	<div id="footer"> <img src="../lib/img/footer.png">
+	<ul class="nav nav-pills">
+    <li><a href="#">Home</a> </li>
+  <li><a href="#">Profil Perencana Pernikahan</a></li>
+  <li><a href="#">Peta Kantor</a></li>
+</ul>
+<img src="../lib/img/footer.png">
