e033de53cccb225427ed0fa2cd06e4b5d5861634
endhitaayu@gmail.com <endhitaayu@gmail.com@8b6feb66-0111-bda6-36b2-7681229f614b>
2012-12-23 04:05:20 +0000

    Menambah profil.php

---
+++ view/profil.php
@@ -0,0 +1,59 @@
+<?php
+include_once ('header.php');
+echo "<div class='span9'>";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<div class="page-header">
+  <h1><center>
+  DhaNeff Online Wedding Planner
+  <br /> 
+  <small>Jasa Pernikahan, Rias Pengantin dan Wedding Planner
+  </small></h1> </center>
+</div>
+
+<div style="margin-left:50px">
+<div class="row">
+<div class="span4"> <img src="../lib/img/profil1.jpg" alt="some_text"> </div>
+<div class="span8"> Jasa Pengantin DhaNeff bergerak di bidang jasa konsultan dan pengadaan peralatan dan perlengkapan
+ jasa pernikahan dan pengantin yang telah berjalan sejak tahun 1990.
+  <br /> DhaNeff adalah perusahaan yang bergerak di bidang jasa yang meliputi jasa pengantin, fotography, video shooting,
+   dan pengadaan jasa konsultasi event weeding dan event2 lainnya. <br /> Pada Mulanya Usaha ini dirintis oleh ibu Hj.Tika 
+   Suradji dengan nama Sanggar Rias Pengantin DhaNeff dan dengan perkembangan waktu, usaha beliau pun berkembang sehingga
+    memiliki legalitas usaha yang jelas. <br /> <br/> <br/> </div> </div>
+	
+<div class="row">
+<div class="span8">
+Kami Sudah mempunyai workshop tersendiri yang anda bisa lihat bila anda berkunjung ke tempat kami yaitu Salon,
+Studio Musik dan Studio Foto antara lain :
+
+<h5> <i class="icon-eye-open"></i> Sanggar Rias Pengantin DhaNeff  <br />
+<i class="icon-headphones"></i> Zack Music Studio - Rental Studio and Sound System <br/>
+<i class="icon-asterisk"></i>Shandy Salon and Bridal </h5> <br/>
+
+Semua Workshop kami tersebut Beralamat di :
+
+
+<address>
+  <strong>DhaNeff Online Wedding Planner</strong><br>
+  Jl. Mekarsari nomer 39
+  Komplek Mekar Indah Melong Asih<br>
+  Bandung<br>
+  <abbr title="Phone">P:</abbr> (022) 70371966
+</address>
+
+ </div>
+
+
+<div class="span4"> <img src="../lib/img/rias.jpg" alt="some_text"> </div>
+
+</div> </div>
+
+
+
+</div> </div>
+<?php
+include_once ('footer.php');
+?>
