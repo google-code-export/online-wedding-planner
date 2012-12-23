0943b77d665417a405fd3e0bf46ba5bd996ea047
nez817 <nez817@8b6feb66-0111-bda6-36b2-7681229f614b>
2012-12-23 07:37:37 +0000

    Issue 5: Daftar Jadwal Temu | model untuk hapus/proses/selesai Daftar Jadwal (Back Office)

---
+++ model/model-daftar-temu.php
@@ -0,0 +1,129 @@
+<?php
+//include('pagination.php'); 
+class daftarTemu{
+	
+	public $mysqli;
+	
+	public $data;
+	
+	public function __construct($host,$username,$password,$db_name){
+		
+		$this->mysqli = new mysqli($host, $username, $password, $db_name);
+		
+		if(mysqli_connect_errno()) {
+ 
+			echo "Error: Could not connect to database.";
+ 
+		exit;
+ 
+		}
+		/*else{
+			echo"Your Database successfully connected";	
+		}*/
+				
+	}
+	
+	public function __destruct(){
+		$this->mysqli->close();	
+	}
+	
+	public function read(){
+		
+		$query="SELECT * FROM planner ORDER by id_status DESC";
+		$result= $this->mysqli->query($query);
+		
+		$per_page = 3;
+		//$p= new Pagination();
+		
+		
+		$num_result=$result->num_rows;
+		//echo $num_result;
+		//$total_results = mysql_num_rows($result);
+        $total_pages = ceil($num_result / $per_page);
+		//echo $total_pages;
+		//$arr = $p->calculate_pages($total_pages, 3, 1);
+
+		
+				
+		if($num_result>0){
+			while($rows=$result->fetch_assoc()){
+								
+				$this->data[]=$rows;
+				
+				//print_r($rows);
+			
+				
+			}
+						
+			return $this->data;
+		}
+		
+		
+	}
+	
+	public function insert($name,$email,$address,$mob){
+		
+		$query="INSERT INTO user SET name='$name', email='$email', address='$address', mob='$mob'";
+		
+		$result= $this->mysqli->query($query) or die(mysqli_connect_errno()."Data cannot inserted");
+		
+		if($result){
+			header('location:read.php');	
+		}
+		
+		
+		
+	}
+	
+	public function proses($id){
+		
+		$query="UPDATE planner SET id_status=2 WHERE Plan_ID='$id'";
+		
+		$result= $this->mysqli->query($query) or die(mysqli_connect_errno()."Data cannot inserted");
+		
+		if($result){
+			//echo 'benar';
+			header('location:../view/daftar-temu.php');	
+		}
+		//else {echo 'salah';}
+		
+	}
+	
+	public function selesai($id){
+		
+		$query="UPDATE planner SET id_status=1 WHERE Plan_ID='$id'";
+		
+		$result= $this->mysqli->query($query) or die(mysqli_connect_errno()."Data cannot inserted");
+		
+		if($result){
+			//echo 'benar';
+			header('location:../view/daftar-temu.php');	
+		}
+		//else {echo 'salah';}
+		
+	}
+	
+	public function delete($id){
+		
+		$query="DELETE FROM planner WHERE Plan_ID='$id'";
+		
+		$result= $this->mysqli->query($query) or die(mysqli_connect_errno()."Data cannot inserted");
+		
+		if($result){
+			//echo 'hapus';
+			header('location:../view/daftar-temu.php');	
+		}
+		//else {echo 'salah';}
+		
+		
+	}
+	
+}
+
+
+//$obj=new Crud("localhost","root","","wedding");
+
+//$obj->read();
+
+
+?>
