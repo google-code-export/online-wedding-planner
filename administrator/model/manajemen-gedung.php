0b3b29dde445d081c845a80ebb8d61b8eb527cce
nez817 <nez817@8b6feb66-0111-bda6-36b2-7681229f614b>
2012-12-29 17:20:03 +0000

    Issue 7: Manajemen Gedung | revisi2 model2 untuk Manajemen Gedung (Back Office)

--- administrator/model/manajemen-gedung.php
+++ administrator/model/manajemen-gedung.php
@@ -21,14 +21,14 @@ class Gedung{
 		$this->mysqli->close();	
 	}
 	
+	//tampilkan semua gedung
 	public function daftar(){
 		
-		$query="SELECT gedung.id_gedung, gedung.nama_gedung, kota.nama_kota, gedung.deskripsi_gedung, gedung.gambar_gedung
-				FROM gedung
-				INNER JOIN kota ON kota.id_kota = gedung.id_kota
-				WHERE gedung.flag=1
-				ORDER BY gedung.id_gedung
-				LIMIT 0 , 30";
+		$query="SELECT g.id_gedung, g.nama_gedung, g.deskripsi_gedung, g.gambar_gedung, g.kapasitas, k.nama_kota
+		FROM gedung g
+		JOIN kota k ON k.id_kota = g.id_kota
+		WHERE g.flag =1
+		ORDER BY g.id_gedung";
 				
 		$result= $this->mysqli->query($query);
 		
@@ -45,9 +45,14 @@ class Gedung{
 		}	
 	}
 	
+	//tampilkan satu gedung
 	public function daftarSatu($id){
 		
-		$query="SELECT * FROM gedung WHERE id_gedung ='$id'";
+		$query="SELECT g.id_gedung, g.nama_gedung, k.nama_kota, k.id_kota, g.deskripsi_gedung, g.gambar_gedung, g.kapasitas
+		FROM gedung g
+		JOIN kota k ON g.id_kota=k.id_kota
+		WHERE id_gedung ='$id'
+		ORDER BY g.id_gedung";
 
 		$result= $this->mysqli->query($query);	
 		
@@ -59,19 +64,21 @@ class Gedung{
 				//print_r($rows);
 			}		
 			return $this->data;
+			
 			//header('location:../view/view-gedung.php');
 		}	
 	}
 	
-	public function detail($id, $detail){
+	//tampilkan detail dari satu gedung
+	public function detail($id){
 		
 		//$query1="SELECT nama_gedung, gambar_gedung, deskripsi_gedung FROM gedung WHERE id_gedung ='$id' LIMIT 0 , 30";
 		
-		$query="SELECT g.nama_gedung, g.gambar_gedung, g.deskripsi_gedung, d.tanggal, d.harga, s.status
+		$query="SELECT d.id_detail, g.nama_gedung, g.gambar_gedung, g.deskripsi_gedung, d.tanggal, d.harga, s.status
 		FROM gedung_detail d
 		JOIN gedung g ON g.id_gedung = d.id_gedung
 		JOIN STATUS s ON s.id_status = d.id_status
-		WHERE g.id_gedung ='$id'
+		WHERE g.id_gedung ='$id' && d.flag = 1
 		ORDER BY d.id_detail
 		LIMIT 0 , 30";
 		$result= $this->mysqli->query($query);	
@@ -81,41 +88,62 @@ class Gedung{
 		join gedung_detail d ON g.id_gedung = d.id_gedung
 		where g.id_gedung ='$id'";
 		$result2=$this->mysqli->query($query2);
-		
-		//print_r($result2->fetch_assoc());
+		//$nama=$result2->fetch_assoc();
+		$a=$result2->fetch_assoc();
+		echo "<h3>".$a['nama_gedung']."</h3>";
 		
 		if($result){
 			while($rows=$result->fetch_assoc()){
 								
 				$this->data[]=$rows;
-				//print_r($rows);
+				//print_r($rows['id_gedung']);
+				
 			}		
 			return $this->data;
+			
 			//header('location:../view/view-gedung.php');
 		}
 	}
 	
-	public function tambah($nama,$kota,$deskripsi,$gambar){
+	//tampilkan form tambah gedung, masukkan input ke db
+	public function tambah($nama_gedung,$id_kota,$deskripsi_gedung, $kapasitas){
 		
-		$query="INSERT INTO gedung SET nama_gedung='$name', id_kota='$kota', deskripsi_gedung='$deskripsi', gambar_gedung='$gambar'";
+		$gambar = $_FILES['image']['name'];
+		
+		$query="INSERT INTO `gedung`(`id_gedung`, `nama_gedung`, `id_kota`, `deskripsi_gedung`,`kapasitas`, `gambar_gedung`, `flag`) 
+		VALUES('', '$nama_gedung', '$id_kota', '$deskripsi_gedung', '$kapasitas', '$gambar', '1')";
 		
 		$result= $this->mysqli->query($query) or die(mysqli_connect_errno()."Data cannot inserted");
 		
 		if($result){
-			header('location:../view/view-gedung.php');	
+			$success_tambah="Sukses tambah data";
+			header('location:../view/view-gedung.php');
+			//$gambar = $_FILES['image']['name'];
+			//echo "$gambar";
 		}
 	}
 	
-	public function ubah($id,$nama,$kota,$deskripsi,$gambar){
+	public function ubahSatu($id_gedung){
 		
-		$query="UPDATE gedung SET nama_gedung='$nama', id_kota='$kota', deskripsi_gedung='$deskripsi', gambar_gedung='$gambar' 
+		$query=$this->daftarSatu($id_gedung);
+		return $query;
+		//print_r($query);
+		//$num_result=$result->num_rows;
+				
+	}
+	
+	public function ubah($id,$nama_gedung,$id_kota,$deskripsi,$kapasitas){
+		
+		$gambar = $_FILES['image']['name'];
+		
+		$query="UPDATE gedung SET nama_gedung='$nama_gedung', id_kota='$id_kota', deskripsi_gedung='$deskripsi', gambar_gedung='$gambar', kapasitas='$kapasitas' 
 		WHERE id_gedung='$id'";
 		
 		$result= $this->mysqli->query($query);
 		
 		if($result){
-			echo 'benar';
-			//header('location:../view/view-gedung.php');	
+			//echo 'benar';
+			header('location:../view/view-gedung.php');	
 		}
 		else {echo 'salah';}
 		
@@ -128,11 +156,30 @@ class Gedung{
 		$result= $this->mysqli->query($query);
 		
 		if($result){
-			echo 'hapus';
+			//echo 'hapus';
 			header('location:../view/view-gedung.php');	
 		}
 		else {echo 'salah';}
 		
+	}
+	
+	public function hapusDetail($id){
+		
+		$query="UPDATE gedung_detail SET flag=0 WHERE id_detail='$id'";
+		$query2="SELECT * from gedung_detail
+		where id_detail ='$id'";
+		$result2=$this->mysqli->query($query2);
+		//$nama=$result2->fetch_assoc();
+		$a=$result2->fetch_assoc();
+		$b= $a['id_gedung'];
+		
+		$result= $this->mysqli->query($query);
+		
+		if($result){
+			//echo 'hapus';
+			header("location:../view/view-gedung-detail.php?id=$b");	
+		}
+		else {echo 'salah';}
 		
 	}
 	
@@ -152,9 +199,24 @@ class Gedung{
 			return $this->data;
 			//header('location:../view/view-gedung.php');
 		}
-		//else {echo 'salah';}
+		//else {echo 'salah';}	
+	}
+	public function opsi(){
 		
-		
+		$query="SELECT * FROM kota";
+		$result= $this->mysqli->query($query) or die(mysqli_connect_errno()."salah");
+		//echo "<select>";
+		if($result){
+			while($rows=$result->fetch_assoc()){
+				$this->data[]=$rows;
+				//$this->data[]=$rows;
+				//echo("<option value='$rows['id_status']'>$rows['status']</option>");
+				//print_r($rows);
+				//echo "<option>".$rows['status']."</option>";
+			}
+			return $this->data;
+		}
+		//echo "</select>";
 	}
 	
 }
@@ -162,7 +224,7 @@ class Gedung{
 
 //$obj=new Gedung("localhost","root","","wedding");
 
-//$obj->detail(1,[]);
-
+//$obj->ubahSatu(1);
+//echo "$obj()";
 
 ?>
--- administrator/model/model-tambah.php
+++ administrator/model/model-tambah.php
@@ -1,21 +1,28 @@
 <?php 
 include('manajemen-gedung.php');
-include_once ('head-back.php');
-echo "<div class='span6 offset1'>";
+//include_once ('head-back.php');
+//echo "<div class='span6 offset1'>";
 
 $obj=new Gedung("localhost","root","","wedding");
-//$gambar_gedung= echo $_FILE['image']['name'];
 
 if(isset($_REQUEST['submit'])){
+	$nama_gedung=$_POST['nama_gedung'];
+	//$id_kota=$_POST['id_kota'];
+	$kapasitas=$_POST['kapasitas'];
+	$deskripsi=$_POST['deskripsi_gedung'];
 	
-	if(isset($_FILES['image']) && $_FILES['image']['size'] > 0){
+	if(empty($nama_gedung) || empty($kapasitas) || empty($deskripsi)){
+			
+		$error_tambah="Data tidak boleh kosong";
+		}
+		if(isset($_FILES['image']) && $_FILES['image']['size'] > 0){
 		
 		//$gambar_gedung= echo $_FILE['image']['name'];
 		
 		extract($_REQUEST);
 	
-		$obj->tambah($nama_gedung, $id_kota, $deskripsi_gedung);
-	
+		$obj->tambah($nama_gedung, $id_kota, $deskripsi_gedung, $kapasitas);
+		
 	}
 }
 ?>
