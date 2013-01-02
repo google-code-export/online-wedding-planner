74af011041110558eb712cb88ae7b41b06178cf7
endhitaayu@gmail.com <endhitaayu@gmail.com@8b6feb66-0111-bda6-36b2-7681229f614b>
2013-01-02 01:20:17 +0000

    modified code

--- view/home.php
+++ view/home.php
@@ -20,17 +20,13 @@ echo "<div class='span9'>";
 			</div>
 			
 			<p class="lead"> <center> <img src="../lib/img/welcome.png"> </center><br>
-			Selamat datang di DhaNeff Online Wedding Planner. Kami adalah sebuah usaha yang bergerak di bidang pelayanan pernikahan yang berdiri sejak tahun 2005 di Yogyakarta.
-
-Melihat begitu pesatnya berkembangan usaha di dunia pernikahan akhir-akhir ini,maka kami hadir dengan membawa konsep yang berbeda.
-
-Kami sangat menyadari bahwa pernikahan adalah salah satu momen yang sangat bermakna bagi kedua mempelai serta bagi semua anggota keluarga besar kedua belah pihak.
-
-Untuk mempersiapkan sebuah pesta pernikahanbanyak hal mesti di lakukan dari pemilihan tempat resepsi,catering,rias,dekorasi,cetak undangan,dll
-
-Demi terlaksananya  sebuah acara,calon pengantin mesti disibukkan untuk mencari paket pernikahan yang terbaik,padahal pekerjaan sehari-hari saja sudah sangat menyita waktu,tenaga,serta pikiran.
-
-Bantuan kerabat memang biasa diandalkan,tetapi apakah mereka benar-benar mempunyai waktu khusus untuk mengurus semuanya?serta benar-benar memahami jasa paket pernikahan yang profesional,mengingat semakin banyaknya paket pernikahan yang bermunculan akhir-akhir ini.
+			Selamat datang di DhaNeff Online Wedding Planner. Kami adalah sebuah usaha yang bergerak di bidang pelayanan pernikahan yang berdiri sejak tahun 2005 di Bandung.
+			Melihat begitu pesatnya berkembangan usaha di dunia pernikahan akhir-akhir ini,maka kami hadir dengan membawa konsep yang berbeda.
+			Kami sangat menyadari bahwa pernikahan adalah salah satu momen yang sangat bermakna bagi kedua mempelai serta bagi semua anggota keluarga besar kedua belah pihak.
+			Untuk mempersiapkan sebuah pesta pernikahan, banyak hal mesti di lakukan dari pemilihan tempat resepsi,catering,rias,dekorasi,cetak undangan,dll.
+			Demi terlaksananya  sebuah acara,calon pengantin mesti disibukkan untuk mencari paket pernikahan yang terbaik,padahal pekerjaan sehari-hari saja sudah sangat menyita waktu,tenaga,serta pikiran.
+			Karena itulah kami hadir. <br> <br>
+			
 			</p>
 			
 			</div>
