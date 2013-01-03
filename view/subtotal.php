cd67801ec37f1dff69f584eba4f8a8988de38ae3
endhitaayu@gmail.com <endhitaayu@gmail.com@8b6feb66-0111-bda6-36b2-7681229f614b>
2013-01-03 02:55:41 +0000

    add subtotal.php

---
+++ view/subtotal.php
@@ -0,0 +1,67 @@
+<?php
+include_once ('header.php');
+echo "<div class='span8'>";
+?>
+
+<!DOCTYPE html>
+<html>
+			<!--form atur jadwal-->
+			<div class='span10 offset2'>
+			<br />
+			<br />
+			<div class="hero-unit">
+			
+			<center><table class="table table-striped">
+  			<caption> <h2> SubTotal </h2></caption>
+  			<thead>
+    		<tr>
+      		<th>Pesan</th>
+			<th>Harga</th>
+    		</tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+	  Gedung</td>
+    </tr>
+	<tr>
+	<td>
+	Paket
+	</td>
+	</tr>
+	<tr>
+	<td>
+	Subtotal
+	</td>
+	</tr>
+	 </tbody>
+</table> </center>
+<br />
+				<table class="table">
+				<tr>
+				<td>
+				<div class='control-group'>
+				<div class='controls'>
+				<button type='submit' class='btn btn-danger'>Kembali ke Daftar Gedung</button>
+				</div>
+				</div>
+				</td>
+				<td>
+				<div class='control-group'>
+				<div class='controls'>
+				<button type='submit' class='btn btn-success'>Lanjut</button>
+				</div>
+				</div>
+				 </td>
+				  </tr>
+				   </table>
+				
+			</form></div></center>
+			</body>
+		
+		</div>
+		<!--End form atur jadwal-->
+</div></div></div>
+<?php
+include_once ('footer.php');
+?>
