9bacdcd1355d02af898a71dc1754541b25b955bc
endhitaayu@gmail.com <endhitaayu@gmail.com@8b6feb66-0111-bda6-36b2-7681229f614b>
2013-01-03 02:54:58 +0000

    add summary.php

---
+++ view/summary.php
@@ -0,0 +1,81 @@
+<?php
+include_once ('header.php');
+echo "<div class='span8'>";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<div class='span10 offset2'>
+<center><img src="../lib/img/summary.png" /></center>
+			<br />
+			<br />
+			<div class="hero-unit">
+			
+				<center><table class="table table-striped">
+				
+  				<tbody>
+    			<tr>
+      			<td> Nama</td>
+    			</tr>
+				<tr>
+				<td> Email </td>
+				</tr>
+				<tr>
+				<td>Jadwal</td>
+				</tr>
+				</tbody>
+				</table> </center>
+				<br />
+				</div>
+				
+				<div class="hero-unit">
+				<center><table class="table table-striped">
+				<thead>
+    		<tr>
+      		<th>Pesan</th>
+			<th>Harga</th>
+    		</tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+	  Gedung</td>
+    </tr>
+	<tr>
+	<td>
+	Paket
+	</td>
+	</tr>
+	<tr>
+	<td>
+	Subtotal
+	</td>
+	</tr>
+	 </tbody>
+</table> </center>
+<br />
+				<table class="table">
+				<tr>
+				<td>
+				</td>
+				<td>
+				<div class='control-group'>
+				<div class='controls'>
+				<button type='submit' class='btn btn-success'>Submit</button>
+				</div>
+				</div>
+				 </td>
+				  </tr>
+				   </table>
+				
+			</form></div></center>
+			</body>
+		</div>
+		</div>
+		
+</div></div></div>
+<?php
+include_once ('footer.php');
+?>
+				
